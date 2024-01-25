3bc509346c18887a82452b0d27f93c79494cf11f
Joshua Peek <josh@joshpeek.com>
2024-01-24 21:41:45 -0700

    Build with dir path to mod

--- release.sh
+++ release.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 set -x
 
 mkdir -p dist/
-GOOS=darwin GOARCH=arm64 go build -o dist/datajar-server cmd/datajar-server/main.go
-GOOS=linux GOARCH=amd64 go build -o dist/datajar-credential-server cmd/datajar-credential-server/main.go
+GOOS=darwin GOARCH=arm64 go build -o dist/datajar-server ./cmd/datajar-server
+GOOS=linux GOARCH=amd64 go build -o dist/datajar-credential-server ./cmd/datajar-credential-server
