685c8d9904082d6f6584bd0d8d5ab006c3cbaa40
Joshua Peek <josh@joshpeek.com>
2024-01-26 19:46:11 -0700

    Try setting timeout

--- internal/shortcuts/scriptingbridge/ShortcutsHelper.m
+++ internal/shortcuts/scriptingbridge/ShortcutsHelper.m
@@ -6,6 +6,7 @@
 int hasShortcut(const char *name) {
     @try {
         ShortcutsApplication *app = [SBApplication applicationWithBundleIdentifier:@"com.apple.shortcuts.events"];
+        app.timeout = 10;
         SBElementArray<ShortcutsShortcut *> *shortcuts = [app shortcuts];
         ShortcutsShortcut *shortcut = [shortcuts objectWithName:[NSString stringWithUTF8String:name]];
 
